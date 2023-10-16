1a6f9a57a351b45ce17325da4e930dcf02ddbaa0
Volker Christian <me@vchrist.at>
2023-10-16 12:22:32 +0200

    Rename -> renumbering

--- rename.sh
+++ rename.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 #
 
-FILES=`ls -1 slides`
+FILES=`ls -1 *.tex | sort`
+
+newNum=0
 
 while IFS='_' read -ra SLIDE <&4 ; do
   SNUMBER="${SLIDE[0]}"
   SNAME="${SLIDE[1]}"
-  ktikz "slides/${SNUMBER}_${SNAME}" > /dev/null 2> /dev/null &
-  read -p "[Name for ${SNUMBER}_slide.tex] " SNNAME
-  echo "   ... rename ${SNUMBER}_slide.tex to ${SNUMBER}_${SNNAME}.tex"
-  mv "slides/${SNUMBER}_slide.tex" "${SNUMBER}_${SNNAME}.tex"
-  killall ktikz
+  newNum=$((newNum+100))
+  num=$(printf "%05d" ${newNum})
+  echo "Moving: ${SNUMBER}_${SNAME} -> ${num}_${SNAME}"
+  mv "${SNUMBER}_${SNAME}" "${num}_${SNAME}"
 done 4<<< "$FILES"
+
